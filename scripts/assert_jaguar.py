b9a9f0979d6c2d55757520a677709f68c945c4eb
NicolasHampa <n_hampa@hotmail.com>
2020-10-28 20:13:50 -0300

    Refactor do script python responsavel pela comparação da cobertura de DUAS obtidas pelas ferramentas Jaguar e BaDua

--- scripts/assert_jaguar.py
+++ scripts/assert_jaguar.py
@@ -2,70 +2,41 @@
 
 import numpy as np
 import os
-import progressbar
 import sys
 import xml.etree.ElementTree as ET
 import csv
 from pathlib import Path
+
+def get_project_name(project_version_jaguar_path):
+	path_split = project_version_jaguar_path.split("/")
+	project_name = path_split[len(path_split)-3]
+	return project_name
+
+def get_project_version(project_version_jaguar_path):
+	path_split = project_version_jaguar_path.split("/")
+	project_version = path_split[len(path_split)-2]
+	return project_version
 
 def line_to_array(line):
 	line = line[:-2].split()
 	a = np.array(list(map(lambda x: int(x), line)))
 	return a
 
-if __name__ == '__main__':
-	project_name = sys.argv[1]
-	project_report_path = os.path.join(Path(project_name).parent.parent.parent.parent, "reports", 'assert_coverage.csv')
-	matrix_folder = os.path.join(project_name, ".jaguar", "matrix")
-	tree = ET.parse(os.path.join(project_name, "badua_report.xml"))
-	badua_root = tree.getroot()
-
-	total_duas = 0
-	jaguar_covered_duas = 0
-	badua_covered_duas = 0
-
-	same_coverage = True
-	for child in badua_root:
-		if child.tag == "class":
-			counters = child.findall("counter[@type='DU']")
-			badua_covered = int(counters[0].attrib["covered"])
-			badua_missed = int(counters[0].attrib["missed"])
-			clazz = child.attrib["name"].replace("/", ".")
-			total = None
-
-			matrix_file = clazz + ".matrix"
-			with open(os.path.join(matrix_folder, matrix_file), "r") as f:
-				for line in f:
-					if line.startswith("=0"): continue
-
-					a = line_to_array(line)
-					if total is None:
-						total = a
-					else:
-						total |= a
-
-			if total is None: 
-				pass
+def count_jaguar_covered_duas(jaguar_matrix_folder_path, matrix_file_name):
+	total = None
+	with open(os.path.join(jaguar_matrix_folder_path, matrix_file_name), "r") as matrix_file:
+		for line in matrix_file:
+			if line.startswith("=0"): continue
+			a = line_to_array(line)
+			if total is None:
+				total = a
 			else:
-				jaguar_covered = np.count_nonzero(total)
-				jaguar_missed = total.size - jaguar_covered
+				total |= a
+	return total
 
-				total_duas += total.size
-				jaguar_covered_duas += jaguar_covered
-				badua_covered_duas += badua_covered
-
-				if badua_covered != jaguar_covered:
-					same_coverage = False
-
+def write_csv(project_report_path, project_name, project_version, total_duas, jaguar_covered_duas, badua_covered_duas):
 	fieldnames = ['PROJECT_NAME', 'PROJECT_VERSION', 'TOTAL_DUAS', 'JAGUAR_COVERED_DUAS', 'BADUA_COVERED_DUAS', 'SAME_COVERAGE']
 	writemode = 'a' if os.path.exists(project_report_path) else 'w'
-	path_split = project_name.split("/")
-	project_name = path_split[len(path_split)-3]
-	project_version = path_split[len(path_split)-2]
-
-	if not same_coverage:
-		print("Ba-Dua and Jaguar have different coverage rates for %s %s" % (project_name, project_version))
-
 	with open(project_report_path, mode=writemode) as coverage_file:
 		csv_writer = csv.DictWriter(coverage_file, fieldnames=fieldnames)
 		if writemode == 'w': csv_writer.writeheader()
@@ -75,5 +46,46 @@ if __name__ == '__main__':
 			'TOTAL_DUAS':total_duas,
 			'JAGUAR_COVERED_DUAS':jaguar_covered_duas,
 			'BADUA_COVERED_DUAS':badua_covered_duas,
-			'SAME_COVERAGE':same_coverage
-		})
+			'SAME_COVERAGE':(badua_covered_duas == jaguar_covered_duas)
+		}
+	)
+
+if __name__ == '__main__':
+	project_version_jaguar_path = sys.argv[1]
+
+	project_name = get_project_name(project_version_jaguar_path)
+	project_version = get_project_version(project_version_jaguar_path)
+
+	project_report_path = os.path.join(Path(project_version_jaguar_path).parent.parent.parent.parent, "reports", 'assert_coverage.csv')
+	jaguar_matrix_folder_path = os.path.join(project_version_jaguar_path, ".jaguar", "matrix")
+	badua_report_file_path = ET.parse(os.path.join(project_version_jaguar_path, "badua_report.xml"))
+	badua_xml_root = badua_report_file_path.getroot()
+
+	total_duas = 0
+	jaguar_covered_duas = 0
+	badua_covered_duas = 0
+	for child in badua_xml_root:
+		if child.tag == "class":
+			counters = child.findall("counter[@type='DU']")
+			badua_covered = int(counters[0].attrib["covered"])
+			badua_missed = int(counters[0].attrib["missed"])
+
+			matrix_file_name = child.attrib["name"].replace("/", ".") 
+			matrix_file_name = matrix_file_name + ".matrix"
+			dua_coverage_array = count_jaguar_covered_duas(jaguar_matrix_folder_path, matrix_file_name)
+
+			if dua_coverage_array is None: 
+				pass
+			else:
+				jaguar_covered = np.count_nonzero(dua_coverage_array)
+				jaguar_missed = dua_coverage_array.size - jaguar_covered
+				jaguar_covered_duas += jaguar_covered
+
+				badua_covered_duas += badua_covered
+
+				total_duas += dua_coverage_array.size
+
+	if not (badua_covered_duas == jaguar_covered_duas):
+		print("Ba-Dua and Jaguar have different coverage rates for %s %s" % (project_name, project_version))
+
+	write_csv(project_report_path, project_name, project_version, total_duas, jaguar_covered_duas, badua_covered_duas)
