0643c6fa568caaa78b44b8f09bb519862adc0e3a
NicolasHampa <n_hampa@hotmail.com>
2021-08-30 00:57:14 -0300

    Script para executar rankeamento de todas as versoes do Defects4J geradas pela Gzoltar

---
+++ scripts/score-ranking/generate-scores-for-all-defects4j-versions.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+if [ -z "$1" ] ; then
+    echo "Repository root path is missing! Execution has been stopped."
+    exit 1
+fi
+
+repository_root_path=$1
+
+for project_path in `find $repository_root_path -maxdepth 1 -mindepth 1 -type d`
+do
+    for project_version_path in `find $project_path -maxdepth 1 -mindepth 1 -type d`
+    do
+        echo $project_version_path
+
+        IFS='/' read -ra project_version_path_array <<< "$project_version_path"
+        index=${#project_version_path_array[@]}
+        project_name=${project_version_path_array[((index-2))]}
+        project_version=${project_version_path_array[((index-1))]}
+
+        ./do-sbfl-analysis $project_name $project_version $project_version_path/matrix $project_version_path/spectra
+    done
+done
