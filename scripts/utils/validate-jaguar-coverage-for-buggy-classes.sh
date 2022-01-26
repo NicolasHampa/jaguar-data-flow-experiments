96aa874dc9d2bf4f33b31791de93b4dfc2e76813
NicolasHampa <n_hampa@hotmail.com>
2022-01-25 21:10:00 -0300

    Gerar arquivo de report ao executar a validacao das buggy-classes entre gzoltar e jaguar

--- scripts/utils/validate-jaguar-coverage-for-buggy-classes.sh
+++ scripts/utils/validate-jaguar-coverage-for-buggy-classes.sh
@@ -18,60 +18,57 @@ do
             project_name=${project_version_path_array[((index-2))]}
             project_version=${project_version_path_array[((index-1))]}
 
-            if [ "$project_name" == "Chart" ]; then
+            printf "\n===================================================================\n" >> "$HOME/report2.txt"
+            echo $project_name"/"$project_version >> "$HOME/report2.txt"
+            echo "===================================================================" >> "$HOME/report2.txt"
+            
+            classes=()
+            while read line; do
+                IFS='#' read -ra line_array <<< "$line"
+                clazz=${line_array[((0))]}
 
-                printf "\n===================================================================\n"
-                echo $project_name"/"$project_version
-                echo "==================================================================="
-                
-                classes=()
-                while read line; do
-                    IFS='#' read -ra line_array <<< "$line"
-                    clazz=${line_array[((0))]}
+                IFS='$' read -ra clazz_array  <<< "$clazz"
+                clazz=${clazz_array[((0))]}
 
-                    IFS='$' read -ra clazz_array  <<< "$clazz"
-                    clazz=${clazz_array[((0))]}
+                IFS='/' read -ra clazz_array <<< "$clazz"
+                index=${#clazz_array[@]}
+                clazz=${clazz_array[((index-1))]}
 
-                    IFS='/' read -ra clazz_array <<< "$clazz"
-                    index=${#clazz_array[@]}
-                    clazz=${clazz_array[((index-1))]}
+                clazz=${clazz%".java"}
 
-                    clazz=${clazz%".java"}
+                if [[ ! " ${classes[*]} " =~ " ${clazz} " ]]; then
+                    classes+=($clazz)
+                fi
+            done < ../score-ranking/buggy-lines/$project_name"-"${project_version%"b"}".buggy.lines"
 
-                    if [[ ! " ${classes[*]} " =~ " ${clazz} " ]]; then
-                        classes+=($clazz)
-                    fi
-                done < ../score-ranking/buggy-lines/$project_name"-"${project_version%"b"}".buggy.lines"
+            # echo "${classes[*]}"
 
-                # echo "${classes[*]}"
+            jaguar_spectra_version_path=$project_version_path"/jaguar/.jaguar/spectra"
+            jaguar_spectra_files=()
+            for spectra_file_path in `find $jaguar_spectra_version_path -maxdepth 1 -mindepth 1 -type f`
+            do
+                IFS='/' read -ra spectra_filename_array <<< "$spectra_file_path"
+                index=${#spectra_filename_array[@]}
+                spectra_filename=${spectra_filename_array[((index-1))]}
+                spectra_filename=${spectra_filename%".spectra"}
 
-                jaguar_spectra_version_path=$project_version_path"/jaguar/.jaguar/spectra"
-                jaguar_spectra_files=()
-                for spectra_file_path in `find $jaguar_spectra_version_path -maxdepth 1 -mindepth 1 -type f`
-                do
-                    IFS='/' read -ra spectra_filename_array <<< "$spectra_file_path"
-                    index=${#spectra_filename_array[@]}
-                    spectra_filename=${spectra_filename_array[((index-1))]}
-                    spectra_filename=${spectra_filename%".spectra"}
+                IFS='.' read -ra spectra_filename_array <<< "$spectra_filename"
+                index=${#spectra_filename_array[@]}
+                spectra_filename=${spectra_filename_array[((index-1))]}
 
-                    IFS='.' read -ra spectra_filename_array <<< "$spectra_filename"
-                    index=${#spectra_filename_array[@]}
-                    spectra_filename=${spectra_filename_array[((index-1))]}
+                if [[ ! " ${jaguar_spectra_files[*]} " =~ " ${spectra_filename} " ]]; then
+                    jaguar_spectra_files+=($spectra_filename)
+                fi
+            done
 
-                    if [[ ! " ${jaguar_spectra_files[*]} " =~ " ${spectra_filename} " ]]; then
-                        jaguar_spectra_files+=($spectra_filename)
-                    fi
-                done
+            # echo "${jaguar_spectra_files[*]}"
 
-                # echo "${jaguar_spectra_files[*]}"
-
-                for classname in "${classes[@]}"
-                do
-                    if [[ ! " ${jaguar_spectra_files[*]} " =~ " ${classname} " ]]; then
-                        echo "Jaguar data-flow coverage for $classname class not found!"
-                    fi
-                done
-            fi
+            for classname in "${classes[@]}"
+            do
+                if [[ ! " ${jaguar_spectra_files[*]} " =~ " ${classname} " ]]; then
+                    echo "Jaguar data-flow coverage for $classname class not found!" >> "$HOME/report2.txt"
+                fi
+            done
         fi
     done
 done
