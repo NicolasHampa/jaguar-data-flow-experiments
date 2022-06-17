4b2a7cf1a6884522d739f8a9b2294cabe43847c5
NicolasHampa <n_hampa@hotmail.com>
2022-06-17 12:02:30 -0300

    Parametros adicionais para o script de avaliação de tecnicas

--- scripts/score-ranking/result-analysis/count-techniques-first-position.py
+++ scripts/score-ranking/result-analysis/count-techniques-first-position.py
@@ -12,13 +12,17 @@ if __name__ == '__main__':
   import pandas as pd
 
   parser = argparse.ArgumentParser()
-  parser.add_argument('--reports_path', required=True, help='path containing the scores')
+  parser.add_argument('--reports_path', required=True, help='root reports path containing the unified scores')
+  parser.add_argument('--scores_filename', required=True, help='csv scores unified filename')
+  parser.add_argument('--output', required=True, help='csv output filename')
 
   args = parser.parse_args()
 
   reports_path = args.reports_path
+  scores_filename = args.scores_filename
+  output = args.output
 
-  with open(os.path.join(reports_path, 'fl-techniques-evaluation.csv'), mode='w') as csv_file:
+  with open(os.path.join(reports_path, output + '.csv'), mode='w') as csv_file:
     fieldnames = ['Technique', 'FirstPosition']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
@@ -26,9 +30,9 @@ if __name__ == '__main__':
     sbfl,mlfl,sbfl_dua,mlfl_dua = 0, 0, 0, 0
     
     for root, dirs, files in os.walk(reports_path):
-      if root[len(reports_path):].count(os.sep) == 2:
+      if root[len(reports_path):].count(os.sep) == 1:
         # reading the csv file
-        scores_sorted = pd.read_csv(os.path.join(root, 'scores-stmt-dua-sorted-v2.csv'), sep=",")
+        scores_sorted = pd.read_csv(os.path.join(root, scores_filename + '.csv'), sep=",")
         best_ranked = scores_sorted.iloc[:1]['Family'].values[0]
         
         if best_ranked == 'sbfl':
