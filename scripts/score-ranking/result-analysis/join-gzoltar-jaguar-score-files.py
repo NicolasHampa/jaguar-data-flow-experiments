3e2c3b6252eeae254bc5d8e9fab2160df8cf182e
NicolasHampa <n_hampa@hotmail.com>
2022-06-17 11:27:51 -0300

    Parametros adicionais para o arquivo de junção de scores

--- scripts/score-ranking/result-analysis/join-gzoltar-jaguar-score-files.py
+++ scripts/score-ranking/result-analysis/join-gzoltar-jaguar-score-files.py
@@ -10,21 +10,23 @@ if __name__ == '__main__':
   import pandas as pd
 
   parser = argparse.ArgumentParser()
-  parser.add_argument('--gzoltar_reports_path', required=True, help='path containing the gzoltar scores')
-  #parser.add_argument('--jaguar_reports_path', required=True, help='path containing the jaguar scores')
+  parser.add_argument('--gzoltar_reports_path', required=True, help='full path for gzoltar reports folder')
+  parser.add_argument('--jaguar_reports_folder', required=True, help='name of jaguar reports folder')
+  parser.add_argument('--output', required=True, help='csv output filename')
 
   args = parser.parse_args()
 
   gzoltar_reports_path = args.gzoltar_reports_path
-  #jaguar_reports_path = args.jaguar_reports_path
+  jaguar_reports_folder = args.jaguar_reports_folder
+  output = args.output
   
   df_gzoltar_scores_sorted = DataFrame()
   for root, dirs, files in os.walk(gzoltar_reports_path):
     if root[len(gzoltar_reports_path):].count(os.sep) == 2:
       jaguar_root = str(root).rsplit('/')
-      jaguar_root[-3]='reports-jaguar'
+      jaguar_root[-3]=jaguar_reports_folder
       df_gzoltar_scores_sorted = pd.read_csv(os.path.join(root, 'scores-sorted.csv'), sep=",")
       df_jaguar_scores_sorted = pd.read_csv(os.path.join('/'.join(jaguar_root), 'scores-sorted.csv'), sep=",")
       df_unified_scores = pd.concat([df_gzoltar_scores_sorted, df_jaguar_scores_sorted])
       df_unified_scores_sorted = df_unified_scores.sort_values(by=['Score'], ascending=True)
-      df_unified_scores_sorted.to_csv(str(root) + '/scores-stmt-dua-sorted.csv', sep=',', index=False)
+      df_unified_scores_sorted.to_csv(str(root) + '/' + output + '.csv', sep=',', index=False)
