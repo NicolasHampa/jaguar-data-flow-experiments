e6fc74d63a56334e19dc19800e321d5c7fa80490
NicolasHampa <n_hampa@hotmail.com>
2022-10-12 23:34:46 -0300

    Adição de train_test_split para otimizar generalização do modelo

--- scripts/score-ranking/mlp-network-fl-analysis.py
+++ scripts/score-ranking/mlp-network-fl-analysis.py
@@ -12,11 +12,10 @@ import matplotlib.pyplot as plot
 
 from pytorchtools import EarlyStopping
 from torch.utils.data import DataLoader
-from torch.utils.data import SubsetRandomSampler
 from torch.optim import Adam
 from torch.optim.lr_scheduler import ReduceLROnPlateau
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import KFold
 
 class MultilayerPerceptron(neural_network.Module):
     def __init__(self, input_size, output_size=1):
@@ -38,20 +37,10 @@ class MultilayerPerceptron(neural_network.Module):
         X = self.fc_layer_4(X)
         return torch.sigmoid(X)
 
-def reset_weights(m):
-    '''
-    Try resetting model weights to avoid
-    weight leakage.
-    '''
-    for layer in m.children():
-        if hasattr(layer, 'reset_parameters'):
-            print(f'Reset trainable parameters of layer = {layer}')
-            layer.reset_parameters()
-
 if __name__ == '__main__':
     torch.manual_seed(101)
     
-    # Load Data    
+    # Parse Args    
     parser = argparse.ArgumentParser()
     parser.add_argument('--matrix', required=True, help='path to the coverage-matrix')
     parser.add_argument('--element-names', required=True, help='file enumerating names for matrix columns')
@@ -60,109 +49,83 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     
-    # Read Program Coverage Data Collected During Execution of Tests Cases
+    # Read Coverage Data Collected During Execution of Tests Cases
     coverage_matrix = pd.read_csv(args.matrix, sep=" ", header=None)
     total_elements = coverage_matrix.shape[1] - 1
     coverage_matrix.iloc[:, total_elements] = coverage_matrix.iloc[:, total_elements].replace(['+','-'],[0,1])
-    coverage_matrix_tensor = torch.tensor(coverage_matrix.values)
-    #test_coverage_data = torch.tensor(coverage_matrix.iloc[:, 0:total_elements].values)
-    #test_execution_results = torch.tensor(coverage_matrix.iloc[:, total_elements].values)
+    test_coverage_data = coverage_matrix.iloc[:, 0:total_elements].values
+    test_execution_results = coverage_matrix.iloc[:, total_elements].values
     
-    # Define the K-fold Cross Validator
-    kfold = KFold(n_splits=10, shuffle=True)
-    
-    # For fold results
-    results = {}
+    # Split (train)
+    x_train, x_test, y_train, y_test = train_test_split(test_coverage_data, test_execution_results, 
+                                                        train_size=0.7, stratify=test_execution_results)
+    train_set = np.c_[x_train, y_train]
+    test_set = np.c_[x_test, y_test]
     
     # Max epochs to iterate
     epochs = 200
+        
+    # Define data loaders for training and testing data in this fold
+    trainloader = DataLoader(train_set, batch_size=128)
+    testloader = DataLoader(test_set, batch_size=32)
     
-    # K-fold Cross Validation model evaluation
-    for fold, (train_ids, test_ids) in enumerate(kfold.split(coverage_matrix_tensor)):
+    # Prepare Model
+    model = MultilayerPerceptron(total_elements)
+    criterion = neural_network.CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr = 0.001)
+    early_stopping = EarlyStopping(patience=20, delta=0.01, verbose=True)
+    scheduler=ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
         
-        # Print
-        print(f'FOLD {fold}')
-        print('--------------------------------')
+    train_losses = []
+    train_correct = []
         
-        # Sample elements randomly from a given list of ids, no replacement.
-        train_subsampler = SubsetRandomSampler(train_ids)
-        test_subsampler = SubsetRandomSampler(test_ids)
+    # Run Model
+    for epoch in range(epochs):
+        train_corr = 0
         
-        # Define data loaders for training and testing data in this fold
-        trainloader = DataLoader(coverage_matrix_tensor, batch_size=128, sampler=train_subsampler)
-        testloader = DataLoader(coverage_matrix_tensor, batch_size=32, sampler=test_subsampler)
-        
-        # Prepare Model
-        model = MultilayerPerceptron(total_elements)
-        model.apply(reset_weights)
-        criterion = neural_network.CrossEntropyLoss()
-        optimizer = Adam(model.parameters(), lr = 0.001)
-        early_stopping = EarlyStopping(patience=20, delta=0.01, verbose=True)
-        scheduler=ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
-        
+        # Train Model
+        for b, train_batch in enumerate(trainloader):
+            # Get inputs
+            inputs = train_batch[:, 0:total_elements]
+            targets = train_batch[:, total_elements]
+            
+            outputs = model(inputs.float())
+            loss = criterion(outputs.squeeze(), targets.float())
+                
+            #predicted = torch.max(y_pred.data, 1)
+            predicted = torch.round(outputs.data).long().squeeze()
+            train_corr = (predicted == targets).sum()
+             
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+                
+            train_losses.append(loss.item())
+            train_correct.append(train_corr)
+                
+        # Calculate average loss over an epoch
+        train_loss = np.average(train_losses)
+        # clear lists to track next epoch
         train_losses = []
-        train_correct = []
         
-        # Run Model
-        for epoch in range(epochs):
-            train_corr = 0
+        epoch_len = len(str(epochs))
+        print_msg = (f'[{epoch:>{epoch_len}}/{epochs:>{epoch_len}}] ' +
+                    f'train_loss: {train_loss:.5f}')
+        print(print_msg)
         
-            # Train Model
-            for b, train_batch in enumerate(trainloader):
-                # Get inputs
-                inputs = train_batch[:, 0:total_elements]
-                targets = train_batch[:, total_elements]
-                
-                outputs = model(inputs.float())
-                loss = criterion(outputs.squeeze(), targets.float())
-                    
-                #predicted = torch.max(y_pred.data, 1)
-                predicted = torch.round(outputs.data).long().squeeze()
-                train_corr = (predicted == targets).sum()
-                    
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                    
-                train_losses.append(loss.item())
-                train_correct.append(train_corr)
-                
-            # Calculate average loss over an epoch
-            train_loss = np.average(train_losses)
-            # clear lists to track next epoch
-            train_losses = []
+        # early_stopping uses the training loss to check if it has decresed
+        early_stopping(train_loss, model)
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
             
-            epoch_len = len(str(epochs))
-            print_msg = (f'[{epoch:>{epoch_len}}/{epochs:>{epoch_len}}] ' +
-                        f'train_loss: {train_loss:.5f}')
-            print(print_msg)
-            
-            # early_stopping uses the training loss to check if it has decresed
-            early_stopping(train_loss, model)
-            if early_stopping.early_stop:
-                print("Early stopping")
-                break
-            
-            scheduler.step(train_loss)
-            
-        # Process is complete.
-        print('Training process has finished. Saving trained model.')
-
-        # Print about testing
-        print('Starting testing')
-    
-        # Saving the model
-        save_path = f'./model-fold-{fold}.pth'
-        torch.save(model.state_dict(), save_path)
-
-        # Evaluationfor this fold
-        correct, total = 0, 0
-            
+        scheduler.step(train_loss)
+        
         # Test Model
+        correct, total = 0, 0
         with torch.no_grad():
             # Iterate over the test data and generate predictions
             for i, test_batch in enumerate(testloader):
-
                 # Get inputs
                 inputs = test_batch[:, 0:total_elements]
                 targets = test_batch[:, total_elements]
@@ -174,20 +137,10 @@ if __name__ == '__main__':
                 predicted = torch.round(outputs.data).long().squeeze()
                 total += targets.size(0)
                 correct += (predicted == targets).sum()
-
-            # Print accuracy
-            print('Accuracy for fold %d: %d %%' % (fold, 100.0 * correct / total))
-            print('--------------------------------')
-            results[fold] = 100.0 * (correct / total)
             
-    # Print fold results
-    print(f'K-FOLD CROSS VALIDATION RESULTS')
-    print('--------------------------------')
-    sum = 0.0
-    for key, value in results.items():
-        print(f'Fold {key}: {value} %')
-        sum += value
-    print(f'Average: {sum/len(results.items())} %')
+            # Print accuracy
+            print('Accuracy: %d %%' % (100.0 * correct / total))
+            print('--------------------------------')
          
     # Build Virtual Coverage Matrix For Fault Prediction
     virtual_coverage_matrix = np.zeros((total_elements, total_elements), dtype=int)
