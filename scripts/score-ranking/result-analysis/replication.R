f7707d323ba80685c1c113ab710176eee125fc98
NicolasHampa <n_hampa@hotmail.com>
2023-04-18 19:54:09 -0300

    Retomada de script do replication para construir tabela para comparação das técnicas

--- scripts/score-ranking/result-analysis/replication.R
+++ scripts/score-ranking/result-analysis/replication.R
@@ -91,3 +91,66 @@ facet_grid(Project~FaultType, scales="free") + labs(x="Absolute score (log scale
 theme + guides + scale_x_log10(breaks = c(1,10,100,1000,10000))
 dev.off()
 embed_fonts(pdfname, options="-dSubsetFonts=true -dEmbedAllFonts=true -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dMaxSubsetPct=100")
+
+
+################################################################################
+# Generate table
+#
+# Cast all scoring metrics to wide format at once
+wide <- dcast(setDT(df), "ID ~ Technique", value.var=scoring_metrics)
+
+#
+# Comparisons
+#
+comparisons <- data.frame(
+  Better=   c("ochiai",    "neural_network", "neural_network"),
+  Worse=    c("tarantula", "ochiai",  "tarantula"))
+
+# Output file for the generated table
+sink(paste(out_dir, "table_replication.tex", sep="/"))
+
+for (i in 1:length(comparisons$Better)) {
+  prior_winner = comparisons$Better[i]
+  prior_loser  = comparisons$Worse[i]
+  
+  # Print the compared techniques
+  cat(sprintf("%12s > %12s",
+              prettifyTechniqueName(prior_winner),
+              prettifyTechniqueName(prior_loser)))
+  
+  # Perform the same test for real faults
+  flt1_score <- paste(metric, prior_winner, sep="_")
+  flt2_score <- paste(metric, prior_loser, sep="_")
+    
+  # Perform a paired, two-tailed t-test
+  t_test <- t.test((wide[[flt1_score]]), (wide[[flt2_score]]), paired=TRUE)
+  p      <- t_test$p.value
+  # Determine the confidence interval (lower, upper)
+  est    <- t_test$estimate
+  lwr    <- t_test$conf.int[1]
+  upr    <- t_test$conf.int[2]
+  # Compute Cohen's d effect size
+  d      <- cohen.d((wide[[flt1_score]]), (wide[[flt2_score]]), paired=TRUE)$estimate
+    
+  # Formatted text for: do we agree, based on p value and effect size?
+  sig_text    <- significanceText(p, est)
+  effect_text <- dText(round(d, 2))
+  
+  # Format the output
+  cat(sprintf("& %3s & %s & %s", sig_text, effect_text, typesetCI(lwr, upr)))
+  
+  # Determine simple counts: how often is one techqniue better than the other?
+  better <- sum(wide[[flt1_score]] < wide[[flt2_score]])
+  worse  <- sum(wide[[flt1_score]] > wide[[flt2_score]])
+  equal  <- sum(wide[[flt1_score]] == wide[[flt2_score]])
+  
+  # Format the output
+  cat(sprintf("& (%d--%d--%d)", better, equal, worse))
+  
+  # Just to visually inspect the distribution of the exam score differences
+  #plot(density(((wide[[flt1_score]]) - (wide[[flt2_score]]))),
+  #     main=sprintf("ScoreRatio difference: %s vs. %s (%s)", prior_winner, prior_loser, "real faults"))
+  
+  cat("\\\\ \n")
+}
+sink()
